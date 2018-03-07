0ebda3ad9fbac6c89a5e6f3330f4d4b589318c94
Servio Mora <servio.mora19@gmail.com>
2018-03-06 19:45:49 -0500

    added firebase

--- App.js
+++ App.js
@@ -4,8 +4,28 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import Router from './Router';
+import firebase from 'firebase';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDP9MZ1lheIaggcg8rxk7gHbrzjq6s20FA",
+      authDomain: "workout-app-2a5e4.firebaseapp.com",
+      databaseURL: "https://workout-app-2a5e4.firebaseio.com",
+      projectId: "workout-app-2a5e4",
+      storageBucket: "workout-app-2a5e4.appspot.com",
+      messagingSenderId: "463525538817"
+    });
+  }
+
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
